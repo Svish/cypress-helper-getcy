b16c3f9d99cd6c29e34e5f852b071f7b0b3fb555
Torleif Berger <torleif@outlook.com>
2019-07-07 18:41:52 +0200

    Copied command documentation into `dist` so VS Code can show it

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,19 @@
 
 declare namespace Cypress {
   interface Chainable<Subject> {
+    /**
+     * Get one or more DOM elements by `data-cy` attribute.
+     *
+     * @example
+     *
+     *     // In a component
+     *     <button data-cy="some-button">Click me</button>
+     *
+     *     // In your test
+     *     cy.getCy('some-button').click();
+     *
+     * @see https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
+     */
     getCy: <E extends HTMLElement>(
       name: string,
       options?: Partial<Loggable & Timeoutable>
